ebd76fe308a89e37245e16ddd4a570dc75eb8440
ldm0 <liudingming@bytedance.com>
2025-09-30 16:22:31 +0800

    refactor(ColumnHeader): streamline column header rendering by mapping over column definitions

--- cardinal/src/components/ColumnHeader.jsx
+++ cardinal/src/components/ColumnHeader.jsx
@@ -1,29 +1,23 @@
 import React, { forwardRef } from 'react';
 
+const columns = [
+  { key: 'filename', label: 'Filename', className: 'filename-text' },
+  { key: 'path', label: 'Path', className: 'path-text' },
+  { key: 'size', label: 'Size', className: 'size-text' },
+  { key: 'modified', label: 'Modified', className: 'mtime-text' },
+  { key: 'created', label: 'Created', className: 'ctime-text' }
+];
+
 // Column widths are applied via CSS vars on container; no need to pass colWidths prop.
 export const ColumnHeader = forwardRef(({ onResizeStart, onContextMenu }, ref) => (
   <div ref={ref} className='header-row-container'>
     <div className='header-row columns' onContextMenu={onContextMenu}>
-      <span className='filename-text header header-cell'>
-        Filename
-        <span className='col-resizer' onMouseDown={onResizeStart('filename')} />
-      </span>
-      <span className='path-text header header-cell'>
-        Path
-        <span className='col-resizer' onMouseDown={onResizeStart('path')} />
-      </span>
-      <span className='size-text header header-cell'>
-        Size
-        <span className='col-resizer' onMouseDown={onResizeStart('size')} />
-      </span>
-      <span className='mtime-text header header-cell'>
-        Modified
-        <span className='col-resizer' onMouseDown={onResizeStart('modified')} />
-      </span>
-      <span className='ctime-text header header-cell'>
-        Created
-        <span className='col-resizer' onMouseDown={onResizeStart('created')} />
-      </span>
+      {columns.map(({ key, label, className }) => (
+        <span key={key} className={`${className} header header-cell`}>
+          {label}
+          <span className='col-resizer' onMouseDown={onResizeStart(key)} />
+        </span>
+      ))}
     </div>
   </div>
 ));
