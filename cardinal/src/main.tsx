dad66b40434a70936a7a0a5a5d4b0ebc85509867
Donough Liu <ldm2993593805@163.com>
2025-12-15 09:10:42 +0800

    feat(tray): add option for tray icon and defaults to disable

--- cardinal/src/main.tsx
+++ cardinal/src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './i18n/config';
 import App from './App';
 import { initializeAppMenu } from './menu';
-import { initializeTray } from './tray';
 import { initializeGlobalShortcuts } from './utils/globalShortcuts';
 import { initializeThemePreference } from './theme';
 
 initializeThemePreference();
-void initializeTray();
 void initializeGlobalShortcuts();
 void initializeAppMenu();
 
